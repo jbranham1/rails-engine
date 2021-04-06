218cc645ed491e6ce504f52b76f42c70ba2a22ec
Jenny Branham <branhamjenny@gmail.com>
2021-04-06 11:05:18 -0500

    Add tests

--- app/controllers/concerns/findable.rb
+++ app/controllers/concerns/findable.rb
@@ -1,29 +1,37 @@
 module Findable
   extend ActiveSupport::Concern
-  def find
-    if params[:name].blank?
+
+  included do
+    before_action :validate_params
+  end
+
+  def validate_params
+    if name_param.blank?
       render json: '', status: :bad_request
-    else
-      item = findable_class.find_by_name(params[:name])
-      if item
-         render json: findable_serializer.new(item)
-       else
-         render json: NullSerializer.new
-      end
+    end
+  end
+
+  def find
+    resource = findable_class.find_by_name(name_param)
+    if resource
+       render json: findable_serializer.new(resource)
+     else
+       render json: NullSerializer.new
     end
   end
 
   def find_all
-    if params[:name].nil? || params[:name] == ''
-      render json: '', status: :bad_request
-    else
-      render json: findable_serializer.new(findable_class.find_all_by_name(params[:name]))
-    end
+    resources = findable_class.find_all_by_name(name_param)
+    render json: findable_serializer.new(resources)
   end
 
   private
 
   def findable_serializer
     "#{findable_class}Serializer".constantize
+  end
+
+  def name_param
+    params[:name]
   end
 end
